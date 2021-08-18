b1526975b8da5906690149911f5daab6575f0fa9
Julya Brustolin <julyab.marssona@gmail.com>
2021-08-18 09:38:09 -0300

    #

--- src/pages/CreateAnimal/index.tsx
+++ src/pages/CreateAnimal/index.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import { Button } from '../../components/Button';
+import { Input } from '../../components/Input';
+import { Sidebar } from '../../components/Sidebar';
+import { TextArea } from '../../components/TextArea';
+
+import { Container, Content, Form, Title } from './styles';
 
 export function CreateAnimal(){
     return (
-        <h1>regitre seu bichinho</h1>
+        <Container>
+      <Sidebar />
+      <Content>
+        <Form>
+          <Title>Registre sua ONG</Title>
+          <Input label="Nome da ONG" type="text" />
+          <Input label="Email" type="text" />
+          <Input label="Escolha uma senha" type="password" />
+          <Input label="Confirme sua senha" type="password" />
+          <Input label="Nome do responsável" type="text" />
+          <Input label="Telefone" type="text" />
+          <TextArea label="Sobre a ONG - máximo de 300 caracteres" />
+          <Input label="Endereço" type="text" />
+
+          <Button />
+        </Form>
+      </Content>
+    </Container>
     )
 }
--- src/pages/CreateAnimal/styles.ts
+++ src/pages/CreateAnimal/styles.ts
@@ -1,3 +1,43 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    display: flex;
+    background: linear-gradient(155.85deg, #F44A87 45.83%, #FFB930 100%);
+    height: 100%;
+`;
+
+export const Content = styled.div`
+    flex: 1;
+`;
+
+export const Title = styled.h1`
+     width: 100%;
+
+    font-size: 32px;
+    line-height: 34px;
+    color: var(--title);
+    font-weight: 700;
+
+    border-bottom: 1px solid var(--border);
+    margin-bottom: 40px;
+    padding-bottom: 24px;
+    text-align: flex-start;
+    `;
+
+export const Form = styled.div`
+    width: 700px;
+    margin: 64px auto;
+
+    background: var(--white);
+    border: 1px solid var(--border);
+    border-radius: 20px;
+
+    padding: 64px 80px;
+
+    overflow: hidden;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+`;
